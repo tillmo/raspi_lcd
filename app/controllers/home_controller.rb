1531c4a7e6a4e9439967c87220a72e4a1651c5ea
Till Mossakowski <till@communtu.de>
2013-01-24 15:40:24 +0100

    bugfix

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,9 +5,9 @@ class HomeController < ApplicationController
     @device = OURDEVICES.to_a[session[:device].to_i]
   end
   def push_button
-    case :params[:dir]
-      when 'up' then session[:device] = session[:device].to_i - 1
-      when 'down' then session[:device] = session[:device].to_i + 1
+    case params[:dir]
+      when 'up' then session[:device] -= 1
+      when 'down' then session[:device] += 1
       end
     redirect_to '/home/index'
   end
