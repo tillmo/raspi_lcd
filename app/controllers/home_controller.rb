ad37235566c075c7eb6e275e3e4271efeb36f19f
Timo Kohorst <timo-hb@arcor.de>
2013-01-24 15:36:21 +0100

    added cookie handling for buttons

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,14 @@
 class HomeController < ApplicationController
   def index
     session[:menu] = 'main_menu' if session[:menu].blank?
-    session[:device] = OURDEVICES.first[0] if session[:device].blank?
+    session[:device] = 0 if session[:device].blank?
+    @device = OURDEVICES.to_a[session[:device].to_i]
   end
   def push_button
+    case :params[:dir]
+      when 'up' then session[:device] = session[:device].to_i - 1
+      when 'down' then session[:device] = session[:device].to_i + 1
+      end
     redirect_to '/home/index'
   end
 end
